78c2086dcaabd7e8718842f1960b3811197284b4
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-09-21 01:35:36 +0530

    Recycler view with adapter created.

--- app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
+++ app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
@@ -2,10 +2,25 @@ package com.edpub.cred_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CategoriesActivity : AppCompatActivity() {
+
+    private lateinit var moneyLayoutManager: GridLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
+
+
+        moneyLayoutManager = GridLayoutManager(this, 1)
+
+        val moneyAdapter = RecyclerViewItemAdapter(moneyLayoutManager)
+
+        val rvMoney = findViewById<RecyclerView>(R.id.rvMoney)
+        rvMoney.layoutManager = moneyLayoutManager
+        rvMoney.adapter = moneyAdapter
+
+
     }
 }
---
+++ app/src/main/java/com/edpub/cred_assignment/RecyclerViewItemAdapter.kt
@@ -0,0 +1,41 @@
+package com.edpub.cred_assignment
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewItemAdapter(private val layoutManager: GridLayoutManager): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    enum class ViewType {
+        SMALL,
+        DETAILED
+    }
+
+    inner class SimpleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        constructor(parent: ViewGroup): this(LayoutInflater.from(parent.context).inflate(R.layout.rv_descriptive_items, parent, false))
+    }
+
+    inner class DetailedViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        constructor(parent: ViewGroup): this(LayoutInflater.from(parent.context).inflate(R.layout.rv_descriptive_items, parent, false))
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ViewType.DETAILED.ordinal -> DetailedViewHolder(parent)
+            else -> SimpleViewHolder(parent)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder.apply {
+
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return super.getItemViewType(position)
+    }
+
+    override fun getItemCount() = 5
+}
