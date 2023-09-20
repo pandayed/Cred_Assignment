199fe08a8089f32f256c8139e51364d1c0e65881
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-09-21 01:57:40 +0530

    Compact rv item added.

--- app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
+++ app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
@@ -1,17 +1,36 @@
 package com.edpub.cred_assignment
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class CategoriesActivity : AppCompatActivity() {
 
+    private val TAG = "CategoriesActivityTag"
     private lateinit var moneyLayoutManager: GridLayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
 
+        val sViewChanger = findViewById<SwitchMaterial>(R.id.sViewChanger)
+        if(sViewChanger.isChecked){
+            loadCompactUI()
+        }else{
+            loadDescriptiveUI()
+        }
+
+        sViewChanger.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if(isChecked){
+                loadCompactUI()
+            }else{
+                loadDescriptiveUI()
+            }
+        }
 
         moneyLayoutManager = GridLayoutManager(this, 1)
 
@@ -22,5 +41,13 @@ class CategoriesActivity : AppCompatActivity() {
         rvMoney.adapter = moneyAdapter
 
 
+    }
+
+    private fun loadDescriptiveUI(){
+        Log.i(TAG, "LoadDescriptiveUI")
+    }
+
+    private fun loadCompactUI(){
+        Log.i(TAG, "LoadCompactUI")
     }
 }
