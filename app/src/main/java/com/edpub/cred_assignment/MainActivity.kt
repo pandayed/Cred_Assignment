efa37c5784c9e159194d9b66ec71abbf69a99b76
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-09-21 00:43:42 +0530

    Recycler view item created.

--- app/src/main/java/com/edpub/cred_assignment/MainActivity.kt
+++ app/src/main/java/com/edpub/cred_assignment/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.edpub.cred_assignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bGotoCategory = findViewById<Button>(R.id.bGotoCategory)
+        bGotoCategory.setOnClickListener {
+            val intent = Intent(this, CategoriesActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
