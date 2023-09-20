2581b660c79b09166fb1aadf11d6ba6a60590526
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-09-21 00:15:50 +0530

    Basic layout added.

---
+++ app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
@@ -0,0 +1,11 @@
+package com.edpub.cred_assignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+}
