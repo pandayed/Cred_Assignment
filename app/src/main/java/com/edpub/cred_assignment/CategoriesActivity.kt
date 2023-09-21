d060e5c497aff783463c6af952fda23d7391a3b6
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-09-21 10:07:33 +0530

    API call added for data.

--- app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
+++ app/src/main/java/com/edpub/cred_assignment/CategoriesActivity.kt
@@ -1,14 +1,21 @@
 package com.edpub.cred_assignment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.switchmaterial.SwitchMaterial
+import org.json.JSONArray
+import org.json.JSONObject
 
 class CategoriesActivity : AppCompatActivity() {
 
     private val TAG = "CategoriesActivityTag"
+    private val url = "https://api.mocklets.com/p68785/skuSections"
 
     private lateinit var moneyLayoutManager: GridLayoutManager
     private lateinit var moneyAdapter: RecyclerViewItemAdapter
@@ -18,6 +25,7 @@ class CategoriesActivity : AppCompatActivity() {
 
     private lateinit var billsLayoutManager: GridLayoutManager
     private lateinit var billsAdapter: RecyclerViewItemAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_categories)
@@ -33,16 +41,16 @@ class CategoriesActivity : AppCompatActivity() {
 
         val sViewChanger = findViewById<SwitchMaterial>(R.id.sViewChanger)
 
-        if(sViewChanger.isChecked){
+        if (sViewChanger.isChecked) {
             loadCompactUI()
-        }else{
+        } else {
             loadDescriptiveUI()
         }
 
         sViewChanger.setOnCheckedChangeListener { compoundButton, isChecked ->
-            if(isChecked){
+            if (isChecked) {
                 loadCompactUI()
-            }else{
+            } else {
                 loadDescriptiveUI()
             }
         }
@@ -60,9 +68,11 @@ class CategoriesActivity : AppCompatActivity() {
         rvBills.layoutManager = billsLayoutManager
         rvBills.adapter = billsAdapter
 
+        getDataFromApi()
+
     }
 
-    private fun loadDescriptiveUI(){
+    private fun loadDescriptiveUI() {
         moneyLayoutManager.spanCount = 1
         moneyAdapter.notifyItemRangeChanged(0, moneyAdapter.itemCount)
 
@@ -74,7 +84,7 @@ class CategoriesActivity : AppCompatActivity() {
 
     }
 
-    private fun loadCompactUI(){
+    private fun loadCompactUI() {
         moneyLayoutManager.spanCount = 3
         moneyAdapter.notifyItemRangeChanged(0, moneyAdapter.itemCount)
 
@@ -83,5 +93,73 @@ class CategoriesActivity : AppCompatActivity() {
 
         billsLayoutManager.spanCount = 3
         billsAdapter.notifyItemRangeChanged(0, billsAdapter.itemCount)
+    }
+
+    private fun getDataFromApi() {
+        Log.i(TAG, "getDataFromApi: Called")
+        val queue = Volley.newRequestQueue(this)
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                parseJsonData(response)
+            },
+            { error ->
+                Log.e(TAG, "Error: ${error}")
+            }
+        )
+        queue.add(jsonObjectRequest)
+    }
+
+    private fun parseJsonData(response: JSONObject) {
+        val moneySize = DataItems.moneyItems.size
+        DataItems.moneyItems.clear()
+        moneyAdapter.notifyItemRangeRemoved(0, moneySize)
+
+        val benefitsSize = DataItems.benefitsItems.size
+        DataItems.benefitsItems.clear()
+        benefitsAdapter.notifyItemRangeRemoved(0, benefitsSize)
+
+        val billsSize = DataItems.billItems.size
+        DataItems.billItems.clear()
+        billsAdapter.notifyItemRangeRemoved(0, billsSize)
+
+        val data = response.get("data") as JSONArray
+
+        val money = data.getJSONObject(0).getJSONObject("section_properties").getJSONArray("items")
+        for (i in 0..<money.length()) {
+            val displayData = money.getJSONObject(i).getJSONObject("display_data")
+            val currDataItem = RvItemClass(
+                R.drawable.ic_launcher_foreground,
+                displayData.getString("name"),
+                displayData.getString("description")
+            )
+            DataItems.moneyItems.add(currDataItem)
+            moneyAdapter.notifyItemInserted(DataItems.moneyItems.size - 1)
+        }
+
+        val benefits = data.getJSONObject(1).getJSONObject("section_properties").getJSONArray("items")
+        for (i in 0..<benefits.length()) {
+            val displayData = benefits.getJSONObject(i).getJSONObject("display_data")
+            val currDataItem = RvItemClass(
+                R.drawable.ic_launcher_foreground,
+                displayData.getString("name"),
+                displayData.getString("description")
+            )
+            DataItems.benefitsItems.add(currDataItem)
+            benefitsAdapter.notifyItemInserted(DataItems.benefitsItems.size - 1)
+        }
+
+        val bills = data.getJSONObject(2).getJSONObject("section_properties").getJSONArray("items")
+        for (i in 0..<bills.length()) {
+            val displayData = bills.getJSONObject(i).getJSONObject("display_data")
+            val currDataItem = RvItemClass(
+                R.drawable.ic_launcher_foreground,
+                displayData.getString("name"),
+                displayData.getString("description")
+            )
+            DataItems.billItems.add(currDataItem)
+            billsAdapter.notifyItemInserted(DataItems.billItems.size - 1)
+        }
+
     }
 }
--- app/src/main/java/com/edpub/cred_assignment/DataItems.kt
+++ app/src/main/java/com/edpub/cred_assignment/DataItems.kt
@@ -2,14 +2,14 @@ package com.edpub.cred_assignment
 
 object DataItems {
     val moneyItems = mutableListOf<RvItemClass>(
-        RvItemClass(R.drawable.ic_launcher_foreground, "Credit Card", "Pay your credit card bills"),
+
     )
 
     val benefitsItems = mutableListOf<RvItemClass>(
-        RvItemClass(R.drawable.ic_launcher_foreground, "Credit Card", "Pay your credit card bills"),
+
     )
 
     val billItems = mutableListOf<RvItemClass>(
-        RvItemClass(R.drawable.ic_launcher_foreground, "Credit Card", "Pay your credit card bills"),
+
     )
 }
